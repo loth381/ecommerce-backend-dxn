7bb8dbe4bcdbb8d4d8ed689ae6170b09925d82ec
Henri <u19217724@gmail.com>
2024-07-25 01:27:25 -0500

    config

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,2 @@
-module.exports = {
-  'strapi-neon-tech-db-branches': {
-    enabled: true,
-    config: {
-      neonApiKey: "n074tjcw788iijm98t6y32mf6o5fmpioov4qlcf9wqpor5p2wya1ojkea9h7zwkz", // get it from here: https://console.neon.tech/app/settings/api-keys
-      neonProjectName: "ecommerce-Dxn", // the neon project under wich your DB runs
-      neonRole: "neondb_owner", // create it manually under roles for your project first
-      gitBranch: "main" // branch can be pinned via this config option. Will not use branch from git then. Usefull for preview/production deployment
-    }
-  },
-}
+// module.exports = {
+// }
